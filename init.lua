54034b76cc669f6d6e086ff9ae4bfbd3fc47c20f
gyong <gitadmin@gybeongotan.dev>
2025-06-27 00:55:50 +0800

    initial commit

---
+++ init.lua
@@ -0,0 +1,40 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+vim.cmd([[
+  hi Normal guibg=NONE ctermbg=NONE
+  hi NormalNC guibg=NONE ctermbg=NONE
+  hi NvimTreeNormal guibg=NONE ctermbg=NONE
+  hi EndOfBuffer guibg=NONE ctermbg=NONE
+  hi LineNr guibg=NONE ctermbg=NONE
+  hi SignColumn guibg=NONE ctermbg=NONE
+  hi VertSplit guibg=NONE ctermbg=NONE
+  hi StatusLine guibg=NONE ctermbg=NONE
+  hi TabLine guibg=NONE ctermbg=NONE
+]])
+
+-- Optionally, make sure your terminal supports transparency
+vim.opt.termguicolors = true
+vim.g.mapleader = " "
+
+-- Function to get the current file's directory (or './' for new files)
+local function current_file_dir()
+  local dir = vim.fn.expand("%:h")
+  if dir == "" then
+    dir = "."
+  end
+  return dir .. "/"
+end
+
+-- Command-line abbreviation: '%%' becomes current file's directory on ':'
+vim.cmd([[
+  cnoreabbrev <expr> %% getcmdtype() == ':' ? expand('%:h') . '/' : '%%'
+]])
+
+-- Key mappings for editing in the same directory as the current file
+vim.keymap.set("n", "<leader>ew", ":e %%", { noremap = true })
+vim.keymap.set("n", "<leader>ev", ":vs %%", { noremap = true })
+vim.keymap.set("n", "<leader>es", ":sp %%", { noremap = true })
+vim.keymap.set("n", "<leader>et", ":tabe %%", { noremap = true })
+
+-- Mapping to open netrw Explore
+vim.keymap.set("n", "<leader>ed", ":Explore<CR>", { noremap = true, silent = true })
