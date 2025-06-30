cefefb6b8ec84f1ef38ab1831eb43a0b09de95e1
gyong <gitadmin@gybeongotan.dev>
2025-06-30 21:31:04 +0800

    add improvements

--- init.lua
+++ init.lua
@@ -3,13 +3,14 @@ require("config.lazy")
 vim.cmd([[
   hi Normal guibg=NONE ctermbg=NONE
   hi NormalNC guibg=NONE ctermbg=NONE
-  hi NvimTreeNormal guibg=NONE ctermbg=NONE
   hi EndOfBuffer guibg=NONE ctermbg=NONE
   hi LineNr guibg=NONE ctermbg=NONE
   hi SignColumn guibg=NONE ctermbg=NONE
   hi VertSplit guibg=NONE ctermbg=NONE
   hi StatusLine guibg=NONE ctermbg=NONE
   hi TabLine guibg=NONE ctermbg=NONE
+  hi NeoTreeNormal guibg=NONE ctermbg=NONE
+  hi NeoTreeNormalNC guibg=NONE ctermbg=NONE
 ]])
 
 -- Optionally, make sure your terminal supports transparency
@@ -38,3 +39,4 @@ vim.keymap.set("n", "<leader>et", ":tabe %%", { noremap = true })
 
 -- Mapping to open netrw Explore
 vim.keymap.set("n", "<leader>ed", ":Explore<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_hl(0, "LspInlayHint", { bg = "NONE", fg = "#888888", italic = true })
